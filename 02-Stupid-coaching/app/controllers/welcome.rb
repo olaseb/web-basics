126e51b3a5a91e4dbd6b56a135c327f7994dfdc0
ola <ola.seb@hotmail.fr>
2014-02-05 14:35:41 +0100

    Sinatra entrîneur stupide

--- 02-Stupid-coaching/app/controllers/welcome.rb
+++ 02-Stupid-coaching/app/controllers/welcome.rb
@@ -1,5 +1,5 @@
 get '/' do
-  @coach_message = params[:coach_message] || "what do you want kid ?"
+  @coach_message = params[:coach_message] || "What is your year of birth ?"
   # Look in app/views/index.erb
   
   puts "self : #{self}"
@@ -8,8 +8,24 @@ get '/' do
   erb :index
 end
 
+
+
 post '/coach' do
   puts "self instance variable from POST '/coach' route handler: #{self.instance_variables}"
   
-  "Implement your coach response ! params => #{params.inspect}"
+
+  @years = params[:years]
+
+  if @years.empty?
+    @coach_message = "Is not ashamed"
+  end
+
+  if params[:verification].include? ('yes')
+    @coach_label2 = "Ok! you have a good #{@years} years"
+  else
+    @coach_label2 =  "Responds yes or no !"
+  end
+
+
+  erb :index
 end
